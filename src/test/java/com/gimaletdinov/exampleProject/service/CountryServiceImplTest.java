f79e426262208d221d096d366d12beb2bb1e51c4
Aidar Gimaletdinov <aidar_008@bk.ru>
2022-04-22 18:06:18 +0500

    переделал тест CountryServiceImplTest

--- src/test/java/com/gimaletdinov/exampleProject/service/CountryServiceImplTest.java
+++ src/test/java/com/gimaletdinov/exampleProject/service/CountryServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.gimaletdinov.exampleProject.service;
 
 import com.gimaletdinov.exampleProject.controller.handler.exceptions.NoSuchObjectException;
+import com.gimaletdinov.exampleProject.dao.CountryRepository;
 import com.gimaletdinov.exampleProject.dto.response.CountryListResponseDto;
 import com.gimaletdinov.exampleProject.dto.response.DocumentTypeListResponseDto;
 import com.gimaletdinov.exampleProject.model.Country;
@@ -8,17 +9,28 @@ import com.gimaletdinov.exampleProject.model.DocumentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static com.gimaletdinov.exampleProject.Helper.UserTestHelper.TEST_COUNTRY_ID;
+import static com.gimaletdinov.exampleProject.Helper.UserTestHelper.getPopulateCountry;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 class CountryServiceImplTest {
+
+    private Country newTestCountry = getPopulateCountry();
+
+    @MockBean
+    private CountryRepository countryRepository;
 
     @Autowired
     private CountryService countryService;
@@ -26,15 +38,30 @@ class CountryServiceImplTest {
     @Test
     @Transactional
     void getAllCountries() {
+        //Given
+        Country newCountry = getPopulateCountry();
+        List<Country> countryList = new ArrayList<>();
+        countryList.add(newCountry);
+        when(countryRepository.findAll()).thenReturn(countryList);
+
+        //When
         List<CountryListResponseDto> resultList = countryService.getAllCountries();
+
+        //Then
         assertTrue(resultList.size() > 0);
     }
 
     @Test
     @Transactional
     void getCountryById() {
-        Country country = countryService.getCountryById(643);
-        assertEquals(country.getId(), 643);
-        assertNotNull(country);
+        //Given
+        when(countryRepository.findById(TEST_COUNTRY_ID)).thenReturn(Optional.ofNullable(newTestCountry));
+
+        //When
+        Country countryFromService = countryService.getCountryById(TEST_COUNTRY_ID);
+
+        //Then
+        assertEquals(TEST_COUNTRY_ID, countryFromService.getId());
+        assertNotNull(countryFromService);
     }
 }
