4e78d3b912f491746f587f75d9690a3a1aa9157a
Aidar Gimaletdinov <aidar_008@bk.ru>
2022-05-06 15:59:22 +0500

    добавил конфигурацию для теста слоя domain

--- domain/src/test/java/com/gimaletdinov/exampleProject/DomainTestConfig.java
+++ domain/src/test/java/com/gimaletdinov/exampleProject/DomainTestConfig.java
@@ -2,7 +2,9 @@ package com.gimaletdinov.exampleProject;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
 
+@TestConfiguration
 @SpringBootConfiguration
 @EnableAutoConfiguration
 public class DomainTestConfig {
